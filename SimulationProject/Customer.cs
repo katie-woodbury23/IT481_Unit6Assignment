1b48eb491a54194c6499f7ee9a7df5fece4a2eb3
Woodb <Woodb@DESKTOP-7C2R75F>
2023-10-29 16:57:37 -0500

    Add project files.

---
+++ SimulationProject/Customer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationProject
+{
+    class Customer
+    {
+        int NumberOfItems; 
+
+        public Customer() 
+        { 
+            NumberOfItems = 6; 
+        }
+
+        public Customer(int items) 
+        { 
+            int ClothingItems = items; 
+            if (ClothingItems == 0) 
+            { 
+                NumberOfItems = GetRandomNumber(1, 20); 
+            } 
+            else 
+            { 
+                NumberOfItems = ClothingItems; 
+            } 
+        } 
+        
+        // Return the number of items
+        public int getNumberOfItems() 
+        { 
+            return NumberOfItems; 
+        } 
+        
+        // Random number methods
+        private static readonly Random getrandom = new Random();
+        
+        public static int GetRandomNumber(int min, int max) 
+        { 
+            lock (getrandom) 
+            {
+                return getrandom.Next(min, max); 
+            } 
+        }
+    }
+}
